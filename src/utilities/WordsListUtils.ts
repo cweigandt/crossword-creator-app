450d88437b3f7060a45d6d5b63849f3d25db6f44
Christian Weigandt <christianrw84@gmail.com>
2021-09-21 22:10:22 -0400

    Faster getWordsThatFit

--- src/utilities/WordsListUtils.ts
+++ src/utilities/WordsListUtils.ts
@@ -1,42 +1,8 @@
-import { getElement, getElementsForRowColumn } from './ElementUtils';
+import { getElement } from './ElementUtils';
 import { SelectionType } from '../data/types/InteractionTypes';
 import { ClueType, ElementType, SolutionType } from '../data/types/PuzzleTypes';
 import { visitSelectionBlocks } from './SolutionUtils';
 
-const doLettersLineUp = (
-  solution: SolutionType,
-  word: string,
-  element: ElementType,
-  elements: ElementType[]
-): boolean => {
-  let theyLineUp = true;
-  visitSelectionBlocks(element, element.length, (row, column, index) => {
-    const possibleElements = getElementsForRowColumn(elements, row, column);
-    let oppositeElement = null;
-    if (possibleElements.length > 0) {
-      if (possibleElements[0].direction === element.direction) {
-        oppositeElement = possibleElements[1];
-      } else {
-        oppositeElement = possibleElements[0];
-      }
-    }
-
-    if (
-      oppositeElement &&
-      oppositeElement.answer !== '' &&
-      word[index] !== solution[row][column]
-    ) {
-      // If there is a crossing element that has an answer filled in
-      // AND the block isn't the same letter as our word
-      // Then we don't have a match
-      theyLineUp = false;
-    }
-  });
-
-  return theyLineUp;
-};
-
-// Includes words that replace non-overlapping blocks
 export const getWordsThatFit = (
   wordsList: ClueType[],
   elements: ElementType[],
@@ -54,16 +20,20 @@ export const getWordsThatFit = (
     return [];
   }
 
-  return wordsList
-    .filter((word) => {
-      return word.answer.replace(/[ -]/g, '').length === selectedElement.length;
-    })
-    .filter((word) => {
-      return doLettersLineUp(
-        solution,
-        word.answer.replace(/[ -]/g, '').toUpperCase(),
-        selectedElement,
-        elements
-      );
-    });
+  let existing = '^';
+  visitSelectionBlocks(
+    selection,
+    selectedElement.length,
+    (row, column, index) => {
+      existing = existing + (solution[row][column] || '.');
+    }
+  );
+
+  existing = existing + '$';
+
+  const regex = new RegExp(existing);
+
+  return wordsList.filter((word) => {
+    return word.answer.replace(/[ -]/g, '').match(regex);
+  });
 };
