cd3c0d7bb8ad35d6970bbfa502f2f0abe374c32d
Christian Weigandt <christianrw84@gmail.com>
2021-09-21 21:45:30 -0400

    RestoreStateModal styling

--- src/utilities/RestoreUtils.ts
+++ src/utilities/RestoreUtils.ts
@@ -1,4 +1,5 @@
-import { PuzzleType } from '../data/types/PuzzleTypes';
+import { Directions } from '../constants/Directions';
+import { ElementType, PuzzleType } from '../data/types/PuzzleTypes';
 
 const hasRequiredProps = (object: object, ...varProps: string[]): boolean => {
   return varProps.reduce((accum, prop) => {
@@ -15,6 +16,28 @@ const validateType = (value: any, name: string, type: string) => {
 const validateLength = (el: any[], len: number, field: string) => {
   if (el.length !== len) {
     throw new Error(`${field} value expected to have length ${len}`);
+  }
+};
+
+const validateTemplateElement = (
+  templateElement: number,
+  printString: string
+) => {
+  validateType(templateElement, printString, 'number');
+  if (templateElement !== 0 && templateElement !== 1) {
+    throw new Error(`'template' values must be either 0 or 1`);
+  }
+};
+
+const validateSolutionElement = (
+  solutionElement: string,
+  printString: string
+) => {
+  validateType(solutionElement, printString, 'string');
+  if (solutionElement.length > 1) {
+    throw new Error(
+      `'solution' values must be either empty or a single character`
+    );
   }
 };
 
@@ -55,22 +78,63 @@ const validateInput = (inputString: string): PuzzleType => {
     for (let columnIndex = 0; columnIndex < jsonPuzzle.width; columnIndex++) {
       const templateElement = templateRow[columnIndex];
       const solutionElement = solutionRow[columnIndex];
-      validateType(
+      validateTemplateElement(
         templateElement,
-        `template[${rowIndex}][${columnIndex}]`,
-        'number'
+        `template[${rowIndex}][${columnIndex}]`
       );
-      validateType(
+      validateSolutionElement(
         solutionElement,
-        `solution[${rowIndex}][${columnIndex}]`,
-        'string'
+        `solution[${rowIndex}][${columnIndex}]`
       );
     }
   }
 
-  // TODO validate elements
-  // TODO validate template is 0 or 1
-  // TODO validate solution is '' or 'A', 'B', etc - 0 or 1 length
+  jsonPuzzle.elements.forEach((el) => {
+    const element = el as ElementType;
+    validateType(element.number, 'elements->number', 'number');
+    validateType(element.row, 'elements->row', 'number');
+    validateType(element.column, 'elements->column', 'number');
+    validateType(element.length, 'elements->length', 'number');
+    validateType(element.direction, 'elements->direction', 'string');
+    validateType(element.clue, 'elements->clue', 'string');
+    validateType(element.answer, 'elements->answer', 'string');
+
+    if (element.number <= 0) {
+      throw new Error(
+        `Element ${element.number} ${element.direction}: number must be positive greater than 0`
+      );
+    }
+
+    if (element.row < 0 || element.row >= jsonPuzzle.height) {
+      throw new Error(
+        `Element ${element.number} ${element.direction}: row must be positive and less than puzzle height`
+      );
+    }
+    if (element.column < 0 || element.column >= jsonPuzzle.width) {
+      throw new Error(
+        `Element ${element.number} ${element.direction}: column must be positive and less than puzzle width`
+      );
+    }
+
+    if (
+      element.length <= 1 ||
+      element.length >= jsonPuzzle.width ||
+      element.length >= jsonPuzzle.height
+    ) {
+      throw new Error(
+        `Element ${element.number} ${element.direction}: length must be positive greater than 1 and less than puzzle's width and height`
+      );
+    }
+
+    if (
+      element.direction !== Directions.ACROSS &&
+      element.direction !== Directions.DOWN
+    ) {
+      throw new Error(
+        `Element ${element.number} ${element.direction}: direction must be either across or down`
+      );
+    }
+  });
 
   return jsonPuzzle;
 };
